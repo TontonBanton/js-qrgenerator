c9b8e6e0a804411608509292b35492cb78f3f36e
TontonBanton <pancla13ulas@gmail.com>
2024-08-07 16:37:01 +0800

    Ver1 Save Download QR

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const qr = document.getElementById('qrcode')
 
 const onGenerateSubmit = (e) => {
   e.preventDefault()
+  clearUI()
+
   const url = document.getElementById('url').value
   const size = document.getElementById('size').value
 
@@ -13,15 +15,13 @@ const onGenerateSubmit = (e) => {
     setTimeout(()=> {
       hideSpinner()
       generateQRCode(url, size)
+      //createsavebtn
+      setTimeout(() => {
+        const saveurl = qr.querySelector('img').src
+        createSaveBtn(saveurl)
+      }, 50);
     },1500)
   }
-}
-
-const showSpinner = ()=> {
-  document.getElementById('spinner').style.display = 'block'
-}
-const hideSpinner = ()=> {
-  document.getElementById('spinner').style.display = 'none'
 }
 
 const generateQRCode = (url, size)=> {
@@ -30,6 +30,24 @@ const generateQRCode = (url, size)=> {
     width: size,
     height: size,
   })
+}
+
+const createSaveBtn = (saveurl)=> {
+  const link = document.createElement('a')
+    link.id = 'save-link'
+    link.classList = 'bg-orange-400 rounded w-1/3 text-white py-2 m-auto my-5 hover:bg-orange-500'
+    link.href = saveurl
+    link.download = 'qrccode'
+    link.innerHTML = 'Save Image'
+  document.getElementById('generated').appendChild(link)
+}
+
+const showSpinner = ()=> { document.getElementById('spinner').style.display = 'block'}
+const hideSpinner = ()=> { document.getElementById('spinner').style.display = 'none'}
+const clearUI = ()=> {
+  qr.innerHTML = ''
+  const saveBtn = document.getElementById('save-link')
+  if (saveBtn) saveBtn.remove()
 }
 
 hideSpinner()
